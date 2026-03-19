d3a9e0ae331f5aa60833c5a3a99d29c5c4694902
Nathan <an.email.com>
2026-03-19 23:59:05 +0000

    revert bug

--- python/stock_trek/data/__init__.py
+++ python/stock_trek/data/__init__.py
@@ -1,6 +1,6 @@
 """Data structures for time-series analysis."""
 
-from .._stock_trek.data import Datum, TimeSeries # type: ignore
+from .._stock_trek import Datum, TimeSeries # type: ignore
 
 __all__ = [
     "Datum",
