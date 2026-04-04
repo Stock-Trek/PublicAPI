5030b492bfab5785c0efb5ccd6c9448a06c6ab97
Nathan <an.email.com>
2026-04-04 13:25:41 +0100

    update python statistics bindings

--- src/bindings/python/statistics/exponential_smoothing.rs
+++ src/bindings/python/statistics/exponential_smoothing.rs
@@ -1,37 +1,47 @@
 #[cfg(feature = "python")]
-use pyo3::pyfunction;
+use {
+    crate::statistics::exponential_smoothing,
+    pyo3::{pyclass, pymethods},
+};
 
 #[cfg(feature = "python")]
-#[pyfunction]
-pub fn simple_exponential_smoothing(time_series_values: Vec<f64>, alpha: f64) -> Vec<f64> {
-    crate::statistics::exponential_smoothing::simple_exponential_smoothing(
-        &time_series_values,
-        alpha,
-    )
-}
+#[pyclass(name = "ExponentialSmoothing")]
+pub struct PyExponentialSmoothing;
 
 #[cfg(feature = "python")]
-#[pyfunction]
-pub fn holt_linear_trend(time_series_values: Vec<f64>, alpha: f64, beta: f64) -> Vec<f64> {
-    crate::statistics::exponential_smoothing::holt_linear_trend(&time_series_values, alpha, beta)
-}
-
-#[cfg(feature = "python")]
-#[pyfunction]
-pub fn holt_winters(
-    time_series_values: Vec<f64>,
-    alpha: f64,
-    beta: f64,
-    gamma: f64,
-    season_len: usize,
-    multiplicative: bool,
-) -> Vec<f64> {
-    crate::statistics::exponential_smoothing::holt_winters(
-        &time_series_values,
-        alpha,
-        beta,
-        gamma,
-        season_len,
-        multiplicative,
-    )
+#[pymethods]
+impl PyExponentialSmoothing {
+    pub fn holt_linear_trend(
+        &self,
+        time_series_values: Vec<f64>,
+        alpha: f64,
+        beta: f64,
+    ) -> Vec<f64> {
+        exponential_smoothing::holt_linear_trend(&time_series_values, alpha, beta)
+    }
+    pub fn holt_winters(
+        &self,
+        time_series_values: Vec<f64>,
+        alpha: f64,
+        beta: f64,
+        gamma: f64,
+        season_len: usize,
+        multiplicative: bool,
+    ) -> Vec<f64> {
+        exponential_smoothing::holt_winters(
+            &time_series_values,
+            alpha,
+            beta,
+            gamma,
+            season_len,
+            multiplicative,
+        )
+    }
+    pub fn simple_exponential_smoothing(
+        &self,
+        time_series_values: Vec<f64>,
+        alpha: f64,
+    ) -> Vec<f64> {
+        exponential_smoothing::simple_exponential_smoothing(&time_series_values, alpha)
+    }
 }
--- src/bindings/python/statistics/time_series.rs
+++ src/bindings/python/statistics/time_series.rs
@@ -1,34 +1,35 @@
 #[cfg(feature = "python")]
-use pyo3::{exceptions::PyRuntimeError, pyfunction, PyResult};
+use {
+    crate::statistics::time_series,
+    pyo3::{exceptions::PyRuntimeError, pyclass, pymethods, PyResult},
+};
 
 #[cfg(feature = "python")]
-#[pyfunction]
-pub fn autocovariance(values: Vec<f64>, lag: usize) -> PyResult<f64> {
-    let result = crate::statistics::time_series::autocovariance(&values, lag)
-        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
-    Ok(result)
-}
+#[pyclass(name = "TimeSeries")]
+pub struct PyTimeSeries;
 
 #[cfg(feature = "python")]
-#[pyfunction]
-pub fn autocorrelation(values: Vec<f64>, lag: usize) -> PyResult<f64> {
-    let result = crate::statistics::time_series::autocorrelation(&values, lag)
-        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
-    Ok(result)
-}
-
-#[cfg(feature = "python")]
-#[pyfunction]
-pub fn partial_autocorrelation(values: Vec<f64>, max_lag: usize) -> PyResult<Vec<f64>> {
-    let result = crate::statistics::time_series::partial_autocorrelation(&values, max_lag)
-        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
-    Ok(result)
-}
-
-#[cfg(feature = "python")]
-#[pyfunction]
-pub fn cross_correlation(first: Vec<f64>, second: Vec<f64>, lag: isize) -> PyResult<f64> {
-    let result = crate::statistics::time_series::cross_correlation(&first, &second, lag)
-        .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
-    Ok(result)
+#[pymethods]
+impl PyTimeSeries {
+    pub fn autocorrelation(&self, values: Vec<f64>, lag: usize) -> PyResult<f64> {
+        time_series::autocorrelation(&values, lag)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))
+    }
+    pub fn autocovariance(&self, values: Vec<f64>, lag: usize) -> PyResult<f64> {
+        time_series::autocovariance(&values, lag)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))
+    }
+    pub fn cross_correlation(
+        &self,
+        first: Vec<f64>,
+        second: Vec<f64>,
+        lag: isize,
+    ) -> PyResult<f64> {
+        time_series::cross_correlation(&first, &second, lag)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))
+    }
+    pub fn partial_autocorrelation(&self, values: Vec<f64>, max_lag: usize) -> PyResult<Vec<f64>> {
+        time_series::partial_autocorrelation(&values, max_lag)
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))
+    }
 }
