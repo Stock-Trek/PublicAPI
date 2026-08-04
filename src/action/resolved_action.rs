ee5614016773b22cff12480f2137ecaae60bea1f
Nathan <an.email.com>
2026-08-04 16:18:48 +0100

    bugfix

--- src/action/resolved_action.rs
+++ src/action/resolved_action.rs
@@ -1,22 +1,20 @@
 use serde::{Deserialize, Serialize};
-use stock_trek_types::cex::{
-    asset_id::AssetId, cex_id::CexId, order_request::OrderRequest, tag::Tag,
-};
+use stock_trek_types::cex::{asset_id::AssetId, cex_id::CexId, order_request::OrderRequest};
 use strum::Display;
 
 #[derive(Display, Serialize, Deserialize)]
 pub enum ResolvedAction {
-    CancelAllOrders,
-    CancelAllOrdersWithTag {
-        tag: Tag,
-    },
-    CancelAllOrdersInCex {
-        cex_id: CexId,
-    },
-    CancelAllOrdersInCexWithTag {
-        cex_id: CexId,
-        tag: Tag,
-    },
+    // CancelAllOrders,
+    // CancelAllOrdersWithTag {
+    //     tag: Tag,
+    // },
+    // CancelAllOrdersInCex {
+    //     cex_id: CexId,
+    // },
+    // CancelAllOrdersInCexWithTag {
+    //     cex_id: CexId,
+    //     tag: Tag,
+    // },
     PlaceOrder {
         cex_id: CexId,
         order_request: OrderRequest<AssetId, f64>,
